79d15392ef223645aeabb0a17eaef64e2dbc978a
Charlotte Romaniuk <charlotte.romaniuk@gmail.com>
2024-08-27 20:07:36 +0100

    update - make flask app run settings configurable with env vars

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # Run server
 from website import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=os.getenv('FLASK_DEBUG'), host=os.getenv('FLASK_RUN_HOST'), port=os.getenv('FLASK_RUN_PORT'))
 
 
