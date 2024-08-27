07a85f5e4955b0c1f6e0bf24e46db2317589ad48
Charlotte Romaniuk <charlotte.romaniuk@gmail.com>
2024-08-27 19:46:46 +0100

    fix - set content-type to application/json and update legacy SQLAlchemy query.get()

--- website/auth.py
+++ website/auth.py
@@ -9,41 +9,41 @@ auth = Blueprint('auth', __name__)
 @auth.post('/register')
 def register_user():
     if 'username' not in request.json:
-        return jsonify({"error": "Missing username value"}), 403
+        return jsonify({"error": "Missing username value"}), 403, {'Content-Type': 'application/json'}
   
     if 'password' not in request.json:
-        return jsonify({"error": "Missing password value"}), 403
+        return jsonify({"error": "Missing password value"}), 403, {'Content-Type': 'application/json'}
     
     username = request.json['username']
     password = request.json['password']
 
     user_exists = User.query.filter_by(username=username).first()
     if user_exists: 
-        return jsonify({"error": "User already exists"}), 409
+        return jsonify({"error": "User already exists"}), 409, {'Content-Type': 'application/json'}
     
     if username and password:
         new_user = User(username=username, password=generate_password_hash(password))
         db.session.add(new_user)
         db.session.commit()
-        return jsonify({"message": "User created"}), 201
+        return jsonify({"message": "User created"}), 201, {'Content-Type': 'application/json'}
     
-    return jsonify({"error": "422 Unprocessable Entity"}), 422
+    return jsonify({"error": "422 Unprocessable Entity"}), 422, {'Content-Type': 'application/json'}
 
 
 @auth.post('/login')
 def login_user():
     if 'username' not in request.json:
-        return jsonify({"error": "Missing username value"}), 403
+        return jsonify({"error": "Missing username value"}), 403, {'Content-Type': 'application/json'}
   
     if 'password' not in request.json:
-        return jsonify({"error": "Missing password value"}), 403
+        return jsonify({"error": "Missing password value"}), 403, {'Content-Type': 'application/json'}
     
     username = request.json['username']
     password = request.json['password']
 
     user = User.query.filter_by(username=username).first()
 
-    if username == user.username and check_password_hash(user.password, password):
+    if user and check_password_hash(user.password, password):
         access_token = create_access_token(identity=username)
         refresh_token = create_refresh_token(identity=username)
 
@@ -55,4 +55,4 @@ def login_user():
             }
         }), 200
 
-    return jsonify({"error": "Invalid username or password"}), 400
+    return jsonify({"error": "Invalid username or password"}), 400, {'Content-Type': 'application/json'}
--- website/tasks.py
+++ website/tasks.py
@@ -3,8 +3,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from . import db
 from .models import Task
 from .models import User
-from .schemas import TaskSchema, UserSchema
-import json
+from .schemas import TaskSchema
 
 tasks = Blueprint('tasks', __name__)
 
@@ -25,52 +24,52 @@ def add_task():
         db.session.add(new_task)
         db.session.commit()
 
-        return TaskSchema().dumps(new_task)
+        return TaskSchema().dumps(new_task), 201, {'Content-Type': 'application/json'}
 
 @tasks.get('/tasks')
 @jwt_required()
 def get_tasks():
         all_tasks = Task.query.all()
-        return TaskSchema(many=True).dumps(all_tasks)
+        return TaskSchema(many=True).dumps(all_tasks), {'Content-Type': 'application/json'}
 
 @tasks.put('/tasks/<int:task_id>')
 @jwt_required()
 def update_task(task_id):
-        existing_task = Task.query.get(task_id)
+        existing_task = db.session.get(Task, task_id)
         if existing_task:
                 if 'title' not in request.json:
-                        return jsonify({"error": "Missing title value"}), 403
+                        return jsonify({"error": "Missing title value"}), 403, {'Content-Type': 'application/json'}
                 if 'description' not in request.json:
-                        return jsonify({"error": "Missing description value"}), 403
+                        return jsonify({"error": "Missing description value"}), 403, {'Content-Type': 'application/json'}
                 if 'completed' not in request.json:
-                        return jsonify({"error": "Missing completed value"}), 403
+                        return jsonify({"error": "Missing completed value"}), 403, {'Content-Type': 'application/json'}
                 task = Task.query.filter_by(id=task_id).first()
                 task.title = request.json['title']
                 task.description = request.json['description']
                 task.completed = request.json['completed']
                 db.session.add(task)
                 db.session.commit()
-                return TaskSchema().dumps(task), 201
+                return TaskSchema().dumps(task), 201, {'Content-Type': 'application/json'}
         else:
-                return jsonify({"error": f"Note with ID {task_id} not found"}), 404
+                return jsonify({"error": f"Task with ID {task_id} not found"}), 404, {'Content-Type': 'application/json'}
         
 @tasks.delete('/tasks/<int:task_id>')
 @jwt_required()
 def delete_task(task_id):
-        existing_task = Task.query.get(task_id)
+        existing_task = db.session.get(Task, task_id)
         if existing_task:
                 task = Task.query.filter_by(id=task_id).first()
                 db.session.delete(task)
                 db.session.commit()
-                return jsonify({"message": f"Note with ID {task_id} successfully deleted"}), 201
+                return jsonify({"message": f"Task with ID {task_id} successfully deleted"}), 201, {'Content-Type': 'application/json'}
         else:
-                return jsonify({"error": f"Note with ID {task_id} not found"}), 404
+                return jsonify({"error": f"Task with ID {task_id} not found"}), 404, {'Content-Type': 'application/json'}
         
 @tasks.get('/tasks/<int:task_id>')
 def get_task(task_id):
-        existing_task = Task.query.get(task_id)
+        existing_task = db.session.get(Task, task_id)
         if existing_task:
                 task = Task.query.filter_by(id=task_id).first()
         else:
-                return jsonify({"error": f"Note with ID {task_id} not found"}), 404
-        return TaskSchema().dumps(task), 200
+                return jsonify({"error": f"Task with ID {task_id} not found"}), 404, {'Content-Type': 'application/json'}
+        return TaskSchema().dumps(task), 200, {'Content-Type': 'application/json'}
