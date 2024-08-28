37cdd7f96d3b06f8c844eb89f4411259249beb4d
Charlotte Romaniuk <charlotte.romaniuk@gmail.com>
2024-08-28 11:55:04 +0100

    feat - add error handler

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 # Run server
 from website import create_app
 import os
+from werkzeug.exceptions import HTTPException
+import json
 
 app = create_app()
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+#Return JSON instead of HTML for HTTP errors
+    response = e.get_response()
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
 
 if __name__ == "__main__":
     app.run(
